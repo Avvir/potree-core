841e75c4e66e9ee75b3db864316081b6e03ac5bd
Mallory Adams <mallory.adams@hexagon.com>
2026-08-18 09:35:49 -0700

    Bind the methods of requestManager when passing into loadPOC
    This change allows using `this.*` inside the `getUrl` and `fetch`
    methods.

--- source/potree.ts
+++ source/potree.ts
@@ -93,7 +93,7 @@ export class Potree implements IPotree
 			const requestManager = reqManager;
 
 			if (url.endsWith('cloud.js')) {
-				return await loadPOC(url, requestManager.getUrl, requestManager.fetch).then((geometry) => {
+				return await loadPOC(url, requestManager.getUrl.bind(requestManager), requestManager.fetch.bind(requestManager)).then((geometry) => {
 					return new PointCloudOctree(this, geometry, material);
 				});
 			} else if (url.endsWith('metadata.json')) {
